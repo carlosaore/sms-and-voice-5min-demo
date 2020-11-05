e8546643efb6aade9d96f7d6cc7e8c17e842105f
Rachel <rachblondon@gmail.com>
2020-11-05 11:08:28 +0100

    demo

--- index.js
+++ index.js
@@ -3,19 +3,3 @@ const client = require('twilio')(process.env.SID, process.env.AUTH)
 const phoneNumber = '+447411275274'
 const _ = require('lodash')
 
-client.messages
-  .list({
-    to: phoneNumber,
-    dateSent: new Date(Date.UTC(2020, 10, 5, 0, 0, 0))
-  })
-  .then(messages => {
-    _.uniqBy(messages, 'from').forEach(message =>
-      client.calls
-        .create({
-          from: phoneNumber,
-          to: message.from,
-          url :'http://rachblondon.ngrok.io/voice.xml'
-        })
-        .then(call => console.log(call.sid))
-    )
-  })
